729006043018fdd889ec87e280e35a7c6bf1717a
Robert James Clay <jame@rocasa.us>
2010-10-02 19:56:49 +0000

    Import of JAME/FTN-Database-0.19 from CPAN.
    gitpan-cpan-distribution: FTN-Database
    gitpan-cpan-version:      0.19
    gitpan-cpan-path:         JAME/ry-run/FTN-Database-0.19.tar.gz
    gitpan-cpan-author:       JAME
    gitpan-cpan-maturity:     released

--- lib/FTN/Database.pm
+++ lib/FTN/Database.pm
@@ -10,11 +10,11 @@ FTN::Database - FTN SQL Database related operations for Fidonet/FTN related proc
 
 =head1 VERSION
 
-Version 0.18
+Version 0.19
 
 =cut
 
-our $VERSION = '0.18';
+our $VERSION = '0.19';
 
 =head1 DESCRIPTION
 
